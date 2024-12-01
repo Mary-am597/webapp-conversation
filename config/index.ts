b5bacdcf2ece7cbe6137cee662ce58e1a913ba2a
Mary-am597 <myoom366@gmail.com>
2024-12-02 03:48:54 +0400

    Update index.ts
    Focus on everything being good

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en',
+  title: 'Robot teacher',
+  description: 'تطبيق روبوت المعلم للتعليم التفاعلي',
+  copyright: '© 2024 جميع الحقوق محفوظة',
+  privacy_policy: 'https://www.termsfeed.com/live/a09bcee2-6b68-4f3e-ae9e-b3bed5106ffd', // رابط سياسة الخصوصية
+  default_language: 'ar',
 }
 
 export const isShowPrompt = false
